c7a25c68e13c881e5b4bd357be579da364dc7f42
Mukesh Verma <mukesh.verma@neosoftmail.com>
2021-07-16 16:12:28 +0530

    Working On Laravel Crud Mongo App

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $connection = 'mongodb';
+	protected $collection = 'books';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'detail'
+    ];
+    use HasFactory;
+}
